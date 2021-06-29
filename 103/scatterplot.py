4660363e69b4126485c7d0bc15f6659ea9e9f691
vasudha921 <61460922+vasudha921@users.noreply.github.com>
2021-06-29 18:27:00 +0530

    Delete 103 directory

--- 103/scatterplot.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import plotly.express as px
-df = pd.read_csv("Copy+of+data+-+data.csv")
-fig= px.scatter(df, x =  "date", y = "cases", color="country", title = "Number of Covid Cases in each Country")
-fig.show()
